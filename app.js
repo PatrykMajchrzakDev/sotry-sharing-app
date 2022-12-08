1898c17593a1c8c14ba13e775c5f69fe37bc91c1
Patryk Majchrzak <patrykmajchrzakdev@gmail.com>
2022-12-08 20:04:16 +0100

    Added logging in authentication via Google

--- app.js
+++ app.js
@@ -3,13 +3,28 @@ const path = require("path");
 const dotenv = require("dotenv");
 const connectDB = require("./database/database");
 const morgan = require("morgan");
+const passport = require("passport");
 const exphbs = require("express-handlebars");
+const session = require("express-session");
 //Load config file
 dotenv.config({ path: "./config/config.env" });
 
 connectDB();
 
 const app = express();
+
+//express session
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+//passport config
+require("./config/passport")(passport);
+
 //morgan
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
@@ -19,11 +34,16 @@ if (process.env.NODE_ENV === "development") {
 app.engine(".hbs", exphbs.engine({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view-engine", ".hbs");
 
+//Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 //Static folder
 app.use(express.static(path.join(__dirname, "public")));
 
 //Routes
 app.use("/", require("./routes/index"));
+app.use("/auth", require("./routes/auth"));
 
 const PORT = process.env.PORT || 4000;
 
